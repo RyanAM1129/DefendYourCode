730fa3bdb4ea2d7c9256462e063d3014f4c126fc
RyanAM1129 <RyanAMacLeod@hotmail.com>
2022-08-07 22:32:15 -0700

    initial commit

---
+++ src/model/Name.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Name {
+    private final String firstName;
+    private final String lastName;
+
+    public Name(final String theFirstName, final String theLastName){
+        
+    }
+}
